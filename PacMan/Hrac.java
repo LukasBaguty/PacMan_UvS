c9ea8f8f0c85cbead9bdc54691b58d8abe82a147
Lukas Blahut <blahut7@stud.uniza.sk>
2025-11-25 15:12:44 +0100

    len som dal triedy

---
+++ PacMan/Hrac.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Hrac here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hrac {
+    /**
+     * Constructor for objects of class Hrac
+     */
+    public Hrac() {
+        // initialise instance variables
+    }
+}
---
+++ PacMan/Pellet.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Pellet here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pellet {
+    /**
+     * Constructor for objects of class Pellet
+     */
+    public Pellet() {
+        // initialise instance variables
+    }
+}
